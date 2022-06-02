a646399a0782eae75b1035a4b816a9f841e9ec9c
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2022-06-02 17:19:37 -0300

    reaproveitando estrutura

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -4,10 +4,14 @@ import {Post} from './Post'
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post 
+        author="Jonatas Fernandes"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi dolorem similique earum exercitationem amet facilis expedita, dolore officia repudiandae fugiat magnam nam eum adipisci, illum mollitia atque ex libero! Magnam."
+      />
+      <Post 
+      author="Diego Fernandes"
+      content="Um post muito legal."
+      />
     </div>
   )
 }
