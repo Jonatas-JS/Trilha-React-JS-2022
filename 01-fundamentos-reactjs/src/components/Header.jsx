444e607e263c16119fa4d6811bfbaec7a8215da0
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2022-06-03 16:20:32 -0300

    criando header

--- 01-fundamentos-reactjs/src/components/Header.jsx
+++ 01-fundamentos-reactjs/src/components/Header.jsx
@@ -1,8 +1,14 @@
-import './Header.module.css'
+import styles from './Header.module.css'
 
 // USAR ELEMENTOS JS COM LETRA MAIÚSCULA E HTML EM MINÚSCULA
+// styles.header => criei uma variável styles que contém as propriedades existentes no arquivo Header.module.css
+// assim, quando informo styles.header significa que na minha variável styles eu tenho uma propriedade(class) que
+//  vai ter os atributos que dei a ele... color: black; height: 80px; max-whidth: 100px;
+
 export function Header() {
   return (
-    <strong className="header">Ignite Feed</strong>
+    <header className={styles.header}>
+      <strong>Ignite Feed</strong>
+    </header>
   )
 }
