1a43dfa78a70a3fb9c2363cf299236ca62ebc816
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2022-06-02 23:59:22 -0300

    iniciando proj

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,9 +1,13 @@
 // JSX = JavaScript + XML
+import { Header } from './components/Header'
 import {Post} from './Post'
+// importar o arquivo CSS com os atributos
+import './styles.css'
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post 
         author="Jonatas Fernandes"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi dolorem similique earum exercitationem amet facilis expedita, dolore officia repudiandae fugiat magnam nam eum adipisci, illum mollitia atque ex libero! Magnam."
---
+++ 01-fundamentos-reactjs/src/components/Header.jsx
@@ -0,0 +1,8 @@
+import './Header.module.css'
+
+// USAR ELEMENTOS JS COM LETRA MAIÚSCULA E HTML EM MINÚSCULA
+export function Header() {
+  return (
+    <strong className="header">Ignite Feed</strong>
+  )
+}
