677c5ab6764cf1e31538165ae0c0b8127779d5ea
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2022-06-15 17:18:46 -0300

    Ajustando o Post

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -62,9 +62,9 @@ export function Post({ author, publisheAt, content }) {
       <div className={styles.content}>
         {content.map(line => {
           if (line.type === 'paragraph') {
-            return <p key={1}>{line.content}</p>
+            return <p key={line.content}>{line.content}</p>
           } else if (line.type === 'link') {
-            return <p key={2}><a href="#">{line.content}</a></p>
+            return <p key={line.content}><a href="#">{line.content}</a></p>
           }
         })}
       </div>
@@ -90,7 +90,7 @@ export function Post({ author, publisheAt, content }) {
               return <Comment />
             })} */}
        {comments.map(comment => {
-        return <Comment content={comment} />
+        return <Comment key={comment} content={comment} />
        })}
       </div>
     </article>
